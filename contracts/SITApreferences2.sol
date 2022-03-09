f011f7fdb685dc999a4f722ca8742e34d1568971
JoelGibson20 <joel.gibson20@gmail.com>
2022-03-09 14:30:03 +0000

    Modified get and set preferences methods in SITApreferences2
    Modified get and set preferences methods in SITApreferences2 to use a combination of address and secret key as the mapping key. This will be expanded so that secret key is an actual encryption key, and the data retrieved will be encrypted.

--- contracts/SITApreferences2.sol
+++ contracts/SITApreferences2.sol
@@ -1,27 +1,21 @@
 pragma solidity >=0.4.22 <0.8.0;
  
 contract SITApreferences2{
-   // Model a preferences set
-    //struct Preferences { // Might be easier to store preferences as a list or a different structure, particularly when we want to encrypt
-        //uint spatial;
-        //uint identity;
-        //uint temporal;
-        //uint activity;
-    //}
-
-
-    mapping(address => uint[4]) userpreferences;
+    mapping(bytes => uint[4]) userpreferences; // Mapping bytes (address + key) as you cant use struct or array as mapping keys
+    // Need to use bytes to be able to concatenate the address and key, should change later to a particular size bytes when we know address and key combined size
 
     constructor() public{}
 
-    function setPreferences(uint spatial, uint identity, uint temporal, uint activity) public returns(bool success){
+    function setPreferences(uint spatial, uint identity, uint temporal, uint activity, string memory key) public returns(bool success){
       // Require statement here to ensure all dimensions are 0-4
-      userpreferences[msg.sender] = [spatial,identity,temporal,activity];
+      bytes memory keyBytes = abi.encodePacked(key); // Convert string parameter for the secret key to bytes
+      userpreferences[abi.encodePacked(msg.sender, keyBytes)] = [spatial,identity,temporal,activity]; // Store the preference array mapped under address + secret key as the mapping key
       return(true); // Need to return upon success of setting, but need require statement
     }
 
-    function getPreferences() public returns(uint[4] memory preferencesList){
-      return(userpreferences[msg.sender]);
+    function getPreferences(string memory key) public returns(uint[4] memory preferencesList){
+      bytes memory keyBytes = abi.encodePacked(key); // Convert string parameter for the secret key to bytes
+      return(userpreferences[abi.encodePacked(msg.sender, keyBytes)]);
 
     }
 
